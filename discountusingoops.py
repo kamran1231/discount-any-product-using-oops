a489c12aa0b06d95238cd7e93c329c763ecd4e33
kamran khan <48378125+kamran1231@users.noreply.github.com>
2019-09-20 20:04:01 +0530

    Add files via upload

---
+++ discountusingoops.py
@@ -0,0 +1,18 @@
+
+class Laptop:
+    #constructor
+    def __init__(self,brand,model,price):
+        self.brand = brand
+        self.model = model
+        self.price = price
+
+    #method
+    def discount_price(self,num):
+        disc = self.price*(num/100)
+
+        return f'After {num} % discount ammount will be',self.price - disc
+
+#object
+laptop1 = Laptop('HP','kfks',70000)
+print(laptop1.price)
+print(laptop1.discount_price(10))
